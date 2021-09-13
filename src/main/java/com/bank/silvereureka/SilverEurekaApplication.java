8b42acfdfd96c46379eded7cf261c77f41f05ee6
CeaserH <ceaser.r.hernandez@gmail.com>
2021-09-13 07:54:11 -0700

    Initial Commit

---
+++ src/main/java/com/bank/silvereureka/SilverEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.bank.silvereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/silvereureka/SilverEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.silvereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
